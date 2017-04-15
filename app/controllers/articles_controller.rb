204ed787b91807c86e35759ab04bce785a6f1606
Al`mir <darzemanov668@gmail.com>
2017-04-15 18:38:40 +0300

    Add actions show, edit and update and view for edit and other

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,17 +3,29 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
 
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create 
     @article = Article.new(article_params)
     if @article.save
-      redirect_to articles_path
+      redirect_to articles_path(@article)
     else
       render 'new'
     end
+  end
+
+  def update
+    
   end
 
   private
