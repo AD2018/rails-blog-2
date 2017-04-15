2ef88c29a633601f6269990ebd80ae9bba5c13d7
Al`mir <darzemanov668@gmail.com>
2017-04-15 18:12:49 +0300

    Add action create and validation for article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,21 @@ class ArticlesController < ApplicationController
   end
 
   def new
-  
+
+  end
+
+  def create 
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:content)
   end
 end
