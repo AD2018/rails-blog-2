f9227bdbd9080e32e4296f5a23f09954e6672957
Al`mir <darzemanov668@gmail.com>
2017-04-20 13:56:37 +0300

    add gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'articles#index'
+  get 'persons/profile', as: 'user_root'
 
+  root 'articles#index'
+  devise_for :users
   resources :articles do
     resources :comments
   end
